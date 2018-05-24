a9608364c7747458db82606108db5e2174bd5d2d
deepanshu1999 <dadeepagg2@gmail.com>
2018-05-24 23:41:31 +0530

    1st commit

---
+++ app/src/main/java/BackupCare.java
@@ -0,0 +1,29 @@
+import com.example.deepanshuaggarwal.receiptvault.globals;
+
+/**
+ * Created by Deepanshu Aggarwal on 04-04-2018.
+ */
+
+public class BackupCare {
+    public static boolean manager(){
+        
+        return false;
+    }
+    public static void setbackuptime(){
+        if(globals.userchoicebackup.equals("Daily")){
+            //set next time
+        }
+        else if(globals.userchoicebackup.equals("Weekly")){
+
+        }
+        else if(globals.userchoicebackup.equals("Monthly")){
+
+        }
+        else if(globals.userchoicebackup.equals("Never")){
+
+        }
+        else if(globals.userchoicebackup.equals("OnItemAdd")){
+
+        }
+    }
+}
